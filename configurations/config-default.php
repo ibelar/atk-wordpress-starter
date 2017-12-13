5878de54c817bbd36734a6dafb4ac1a5ff0d3d6b
Alain Belair <belair.alain@gmail.com>
2017-12-13 16:40:41 -0500

    update comments

--- configurations/config-default.php
+++ configurations/config-default.php
@@ -4,5 +4,12 @@
  * Plugin configuration.
  */
 
+/**
+ * Enable ajax security.
+ */
 $config['plugin']['use_nounce'] = false;
+
+/**
+ * Enable ajax in front end.
+ */
 $config['plugin']['use_ajax_front'] = true;
